1eb90b19126f4535f552ce9ccc15a73267cf7c8d
Mr.Nadun <mrnadun878@gmail.com>
2025-09-19 14:11:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;uEFk2BBK#uG7UfCe3brUxJeqmAlNUse7F-ZvLRCzKA2DlKgu2jls", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94760405102", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
